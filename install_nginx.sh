814fa3eebe5d2dbc29c421cf618c8ec353d0f89d
anandkumarprajap <anandkumar7738@gmail.com>
2026-07-07 13:54:17 +0530

    added nginx script

---
+++ install_nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
+cp hello.html /var/www/html
+sudo systemctl restart nginx
+echo "Devboard running In Poart 88"
